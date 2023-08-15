78af2792da0d2be29559e79fb00ad05c0531727d
Matthew Evans <me@mjevans.dev>
2023-08-15 09:35:16 -0600

    Fixed unit test with dynamic future date calculation
    - removed non-spec file from spec/ dir

--- calendar-service/app.js
+++ calendar-service/app.js
@@ -3,15 +3,24 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 8000;
 
+function randomHour() {
+  const hour =  Math.floor(Math.random() * 24) + 1;
+  return hour;
+}
+
 function someDaysAgo(days) {
   const currentDate = new Date();
   currentDate.setDate(currentDate.getDate() - days);
+  currentDate.setHours(randomHour());
+  currentDate.setMinutes(0);
   return currentDate;
 }
 
 function someDaysBeyond(days) {
   const currentDate = new Date();
   currentDate.setDate(currentDate.getDate() + days);
+  currentDate.setHours(randomHour());
+  currentDate.setMinutes(0);
   return currentDate;
 }
 
@@ -30,29 +39,33 @@ app.get("/events", (req, res) => {
         id: 1,
         name: "Hangout",
         duration: 30,
-        date: someDaysAgo(3).toLocaleDateString('en-US'),
-        attendees: 2
+        date: someDaysAgo(3).toLocaleString('en-US'),
+        attendees: 2,
+        timeZone: "America/New_York"
       },
       {
         id: 2,
         name: "Pre-Screen",
         duration: 60,
-        date: someDaysAgo(5).toLocaleDateString('en-US'),
-        attendees: 3
+        date: someDaysAgo(5).toLocaleString('en-US'),
+        attendees: 3,
+        timeZone: "America/Chicago"
       },
       {
         id: 3,
         name: "Group Interview",
         duration: 120,
-        date: someDaysAgo(4).toLocaleDateString('en-US'),
-        attendees: 3
+        date: someDaysAgo(4).toLocaleString('en-US'),
+        attendees: 3,
+        timeZone: "America/Denver"
       },
       {
         id: 4,
         name: "1on1",
         duration: 60,
-        date: someDaysBeyond(4).toLocaleDateString('en-US'),
-        attendees: 2
+        date: someDaysBeyond(4).toLocaleString('en-US'),
+        attendees: 2,
+        timeZone: "America/Los_Angeles"
       }
     ]
   })
--- user-service/app/controllers/user_controller.rb
+++ user-service/app/controllers/user_controller.rb
@@ -1,10 +1,10 @@
 class UserController < ApplicationController
   USERS = [
-    {'id' => 1, 'first_name' => 'Michael', 'last_name' => 'Scott', 'position' => 'Regional Manager', 'role' => 'admin'},
-    {'id' => 2, 'first_name' => 'Jim', 'last_name' => 'Halpert', 'position' => 'Salesperson', 'role' => 'user'},
-    {'id' => 3, 'first_name' => 'Pam', 'last_name' => 'Beesly', 'position' => 'Receptionist', 'role' => 'user'},
-    {'id' => 4, 'first_name' => 'Dwight', 'last_name' => 'Schrute', 'position' => 'Salesperson', 'role' => 'user'},
-    {'id' => 5, 'first_name' => 'Angela', 'last_name' => 'Martin', 'position' => 'Accountant', 'role' => 'user'}
+    {'id' => 1, 'first_name' => 'Michael', 'last_name' => 'Scott', 'position' => 'Regional Manager', 'role' => 'admin', 'time_zone' => 'America/New_York'}, 
+    {'id' => 2, 'first_name' => 'Jim', 'last_name' => 'Halpert', 'position' => 'Salesperson', 'role' => 'user', 'time_zone' => 'America/Detroit'},
+    {'id' => 3, 'first_name' => 'Pam', 'last_name' => 'Beesly', 'position' => 'Receptionist', 'role' => 'user', 'time_zone' => 'America/Denver'},
+    {'id' => 4, 'first_name' => 'Dwight', 'last_name' => 'Schrute', 'position' => 'Salesperson', 'role' => 'user', 'time_zone' => 'America/Chicago'},
+    {'id' => 5, 'first_name' => 'Anglea', 'last_name' => 'Martin', 'position' => 'Accountant', 'role' => 'user', 'time_zone' => 'America/Los_Angeles'},
   ]
 
   def show
